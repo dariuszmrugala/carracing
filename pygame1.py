7990a4215d27b898b23188bc95cfedd5218fb284
dariusz <dariuszmrugala@gmail.com>
2021-03-16 17:07:19 +0100

    narrowing path

--- pygame1.py
+++ pygame1.py
@@ -18,15 +18,16 @@ trackRight = []
 SPEED = 4
 trackCount = 0
 trackPosition = 250
-trackWidth = 120
+trackWidth = 200 #poczatkawa wartosc 120
 trackDirection = False
 gameStatus = 0
 
 
 def draw():
     global gameStatus
-    #ekran(128)
+
     screen.fill((0, 250, 0))
+    
     if gameStatus == 0:
         car.draw()
         b = 0
@@ -34,52 +35,51 @@ def draw():
             trackLeft[b].draw()
             trackRight[b].draw()
             b += 1
+            
     if gameStatus == 1:
         screen.blit('rflag', (318,268))
 
-        #gameStatus = 0
-       #tutaj
     if gameStatus == 2:
         screen.blit('cflag', (318,268))
 
-def update():
-    global gameStatus, trackCount, SPEED
-    if gameStatus == 0:
-        if keyboard.left and car.x > 0:  car.x -= CARSPEED
-        if keyboard.right and car.x < WIDTH: car.x += CARSPEED
 
+def update():
+    global gameStatus, trackCount, SPEED, CARSPEED, trackWidth
+    
+    if gameStatus == 0:
+        if keyboard.left and car.x > 0:
+            car.x -= CARSPEED
+        if keyboard.right and car.x < WIDTH:
+            car.x += CARSPEED
+        if trackCount > ILEDOWYGRANIA:
+            gameStatus = 2
         updateTrack()
+        
     if gameStatus == 1:
         if keyboard.space:
             resetGame()
-            
+
     if gameStatus == 2:
         if keyboard.space:
             resetGame()
 
-    if trackCount > ILEDOWYGRANIA: gameStatus = 2
-
-    #print(trackCount)
 
 def makeTrack():
     global trackCount, trackLeft, trackRight, trackPosition,trackWidth, ILEBECZEKNAEKRANIE
+    
     trackLeft.append(Actor("barrier", pos=(trackPosition-trackWidth,0)))
     trackRight.append(Actor("barrier", pos=(trackPosition+trackWidth,0)))
     trackCount += 1
-    #print('ile w kontenerze: ')
-    #print(len(trackLeft))
-
-    #print(trackCount)
-    #print(ILEBECZEKNAEKRANIE)
-
+ 
     if trackCount > ILEBECZEKNAEKRANIE:
-        #print('beczki poza ekranem')
         del trackLeft[0]
         del trackRight[0]
-
+    
 
 def updateTrack():
     global trackCount, trackPosition, trackDirection, trackWidth, gameStatus, SPEED, CARSPEED
+    BarrelHeight = 32
+    
     b = 0
     while b < len(trackLeft):
         if car.colliderect(trackLeft[b]) or car.colliderect(trackRight[b]):
@@ -88,27 +88,33 @@ def updateTrack():
         trackLeft[b].y += SPEED
         trackRight[b].y += SPEED
         b += 1
-    if trackCount == 100:
-        SPEED += 0.1
-        CARSPEED += 0.2
-    if trackCount == 200:
-        SPEED += 0.1
-        CARSPEED += 0.2
-    if trackCount == 300:
-        SPEED += 0.1
-        CARSPEED += 0.2
 
-
-    if trackLeft[len(trackLeft)-1].y > 32:
+    if trackLeft[len(trackLeft)-1].y > BarrelHeight:
         if trackDirection == False: trackPosition += 16
         if trackDirection == True: trackPosition -= 16
         if randint(0,4) == 1: trackDirection = not trackDirection
         if trackPosition > 700-trackWidth: trackDirection = True
         if trackPosition < trackWidth: trackDirection = False
+        
+        if trackCount == 100:
+            SPEED += 1
+            CARSPEED += 1.5
+            trackWidth -= 50
+        if trackCount == 200:
+            SPEED += 1
+            CARSPEED += 1.2
+            trackWidth -= 20
+        if trackCount == 300:
+            SPEED += 1
+            CARSPEED += 1
+            trackWidth -= 20
+
         makeTrack()
 
+
 def resetGame():
-    global gameStatus, trackLeft, trackRight, trackCount, SPEED, CARSPEED, trackPosition, trackDirection
+    global gameSatus, trackLeft, trackRight, trackCount, SPEED, CARSPEED, trackPosition, trackDirection, trackWidth
+    
     gameStatus = 0
     trackLeft.clear()
     trackRight.clear()
@@ -118,6 +124,7 @@ def resetGame():
     car.pos=250,700
     trackPosition = 250
     trackDirection = False
+    trackWidth = 200
     makeTrack()
 
 makeTrack()
